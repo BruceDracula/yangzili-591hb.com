5e781132c5214408a1cbea81f5ef39e91a47aeb2
BruceDracula <yangzili@591hb.com>
2015-03-24 13:37:14 +0800

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+unsigned int a=483274305;
+float b=8.1323;
+//float c=b*10;
+float c=8.1323;
+printf("%u\n",a*666);
+printf("%f\n",b*3732882388899);
+printf("%f\n",c/372834329437432);
+return(0);
+}
